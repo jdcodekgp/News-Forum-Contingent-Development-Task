1ae7322e34520751654404b564529044e4b86612
jdcodekgp <130901474+jdcodekgp@users.noreply.github.com>
2023-10-16 23:32:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Apna News',
+      title: 'News Bite',
       home: Home(),
     );
   }
